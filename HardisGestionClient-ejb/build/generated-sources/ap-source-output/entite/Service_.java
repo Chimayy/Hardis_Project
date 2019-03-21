5cba46bdc7c9fc8a1d7f149b50273c8a03fe3a5f
CheyrouLag <37335323+CheyrouLag@users.noreply.github.com>
2019-03-21 11:57:54 +0100

    Exiless commit without bug and without taking ages to run

--- HardisGestionClient-ejb/build/generated-sources/ap-source-output/entite/Service_.java
+++ HardisGestionClient-ejb/build/generated-sources/ap-source-output/entite/Service_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-21T11:38:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-03-21T11:57:04")
 @StaticMetamodel(Service.class)
 public class Service_ { 
 
